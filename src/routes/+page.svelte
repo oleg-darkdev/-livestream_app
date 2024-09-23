3772ab531d84d5e0c88b2de4c00c028506e7ee7b
DarkDev <darkdev_brand@riseup.net>
2024-09-23 04:08:58 +0300

    clear

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,71 +1,8 @@
 <script>
-	import { Header, Footer, AsideBtns, FeedbackSlider, AnimatedLogo, BackgroundImageAlt, Gallery, Price, Features, About, Faq } from '$widgets';
+	// import { } from '$widgets';
 </script>
 
 <svelte:head>
-	<title>LiveLab</title>
+	<title>LiveLab app</title>
 </svelte:head>
 
-
-<Header />
-
-<AsideBtns />
-
-<BackgroundImageAlt />
-
-<main class="pb-40">
-	<div class="">
-		<Features />
-
-		<About />
-
-		<Price />
-
-		<!-- <Gallery /> -->
-
-
-
-
-
-		<Faq />
-
-		<FeedbackSlider />
-		<!-- <About /> -->
-		<!-- <GettingStarted /> -->
-		<!-- <Navigation /> -->
-		<!-- <BrandColors /> -->
-		<!-- <ColorCombos /> -->
-
-		<!-- <DateTimePickers /> -->
-		<!-- <Buttons /> -->
-		<!-- <Cards /> -->
-		<!-- <Loader /> -->
-
-		<!-- <SourceCode /> -->
-		<!-- <Fonts /> -->
-		<!-- <Attention /> -->
-		<!-- <Inputs /> -->
-		<!-- <Headings /> -->
-		<!-- <Steps /> -->
-		<!-- <Tables /> -->
-
-		<!-- <AltLists /> -->
-		<!-- <Razors /> -->
-		<!-- <Banners /> -->
-		<!-- <GlitchEffect /> -->
-		<!-- <BackgroundImage /> -->
-		<!-- <Aside /> -->
-		<!-- <Email /> -->
-	</div>
-</main>
-
-<AnimatedLogo />
-
-<Footer />
-
-<!-- <GlitchEffect /> -->
-<!-- <GlitchEffect /> -->
-<!-- <GlitchEffect /> -->
-<!-- <GlitchEffect /> -->
-<!-- <GlitchEffect /> -->
-<!-- <GlitchEffect /> -->
